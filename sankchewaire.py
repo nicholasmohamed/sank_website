a24b7f44a51283f30d0b1fe14bb0bc22c360b314
Nicholas Mohamed <nick.mohamed5@gmail.com>
2021-11-19 18:45:45 -0500

    Add debug to launch.

--- sankchewaire.py
+++ sankchewaire.py
@@ -3,7 +3,7 @@ from app.models import SankMerch
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
-    application.run(host='0.0.0.0', port=port)
+    application.run(debug=True, host='0.0.0.0', port=port)
 
 app = create_app()
 
