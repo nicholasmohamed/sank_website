46cf8338c7affe4d58a92d3a268fe320ee4ca2b3
Nicholas Mohamed <nick.mohamed5@gmail.com>
2022-03-01 20:58:11 -0500

    Remove debug mode.

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,9 +1,6 @@
 from flask import Blueprint
 import os
 
-if os.environ.get('DEBUG_MODE') == 'true':
-    bp = Blueprint('main', __name__, static_folder=os.path.join(os.getcwd(), 'app/static'))
-else:
-    bp = Blueprint('main', __name__, static_folder=os.path.join(os.getcwd(), '/static'))
+bp = Blueprint('main', __name__, static_folder=os.path.join(os.getcwd(), 'app/static'))
 
 from app.main import routes
